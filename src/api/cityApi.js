699ebc4cf11f33bda64b184dbb76bf78f1167d6a
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-01-17 21:06:52 +0300

    Installing and testing tanstack query

--- src/api/cityApi.js
+++ src/api/cityApi.js
@@ -1,38 +1,41 @@
 import axios from "axios"
 
 export const fetchCityCoordinates = async (city) => {
-    try {
-        const res = await axios.get('https://geocoding-api.open-meteo.com/v1/search', {
-            params: {
-                name: city,
-                count: 1,
-                language: 'ru'
-            }
-        })
-        return res.data.results[0]        
-    } catch (error) {
-        console.log({error})
+  const res = await axios.get(
+    'https://geocoding-api.open-meteo.com/v1/search',
+    {
+      params: {
+        name: city,
+        count: 1,
+        language: 'ru',
+      },
     }
+  )
+
+  if (!res.data.results?.length) {
+    throw new Error('Город не найден')
+  }
+  console.log(res.data.results[0])
+  return res.data.results[0]
 } 
 
 export const fetchCityWeather = async (city) => {
-    try {
-        const res = await fetchCityCoordinates(city)
-        const latitude = res.latitude
-        const longitude = res.longitude
+  const { latitude, longitude } = await fetchCityCoordinates(city)
 
-        const res2 = await axios.get('https://api.open-meteo.com/v1/forecast',{
-            params: {
-                latitude,
-                longitude,
-                hourly: 'relative_humidity_2m,temperature_2m,weathercode',
-                current_weather: true,
-                timezone: 'auto'
-            }
-        })
-
-        return res2.data        
-    } catch (error) {
-        console.log({error})
+  const res = await axios.get(
+    'https://api.open-meteo.com/v1/forecast',
+    {
+      params: {
+        latitude,
+        longitude,
+        hourly: 'relative_humidity_2m,temperature_2m,weathercode',
+        forecast_days: 1,
+        current_weather: true,
+        timezone: 'auto',
+      },
     }
+  )
+
+  console.log(res.data)
+  return res.data
 }
